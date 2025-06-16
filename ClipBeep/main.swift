09a84286e82563d75c08051a25c1806443516c11
周辉 <faichou.zh@gmail.com>
2025-06-16 11:54:06 +0800

    feat: add clipboard monitoring with sound notifications

--- ClipBeep/main.swift
+++ ClipBeep/main.swift
@@ -6,6 +6,36 @@
 //
 
 import Foundation
+import AppKit
 
-print("Hello, World!")
+/**
+     Basso.aiff     Bottle.aiff    Funk.aiff      Hero.aiff      Ping.aiff      Purr.aiff      Submarine.aiff
+     Blow.aiff      Frog.aiff      Glass.aiff     Morse.aiff     Pop.aiff       Sosumi.aiff    Tink.aiff
+ */
+let BEEP_NAME = "Frog"
 
+class ClipboardMonitor {
+    private var timer: Timer?
+    private var lastChangeCount = NSPasteboard.general.changeCount
+
+    init() {
+        startMonitoring()
+        RunLoop.current.run()
+    }
+
+    private func startMonitoring() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
+            self?.checkClipboard()
+        }
+    }
+
+    private func checkClipboard() {
+        let pasteboard = NSPasteboard.general
+        if pasteboard.changeCount != lastChangeCount {
+            lastChangeCount = pasteboard.changeCount
+            NSSound(named: NSSound.Name("Frog"))?.play()
+        }
+    }
+}
+
+_ = ClipboardMonitor()
